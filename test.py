37f3f9d7099f7b97122137e4ebc9f2f5e33f04f9
Enkidu93 <lowryec17@gcc.edu>
2024-05-13 10:48:15 -0400

    Make example app

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import subprocess
 
-st.title('HELLO')
+st.title('THIS IS AN EXAMPLE')
+if st.button('Run subprocess'):
+    ret = subprocess.run('python script.py')
+    st.write(ret.returncode)
+    st.write(ret.stdout)
+    st.write(ret.stderr)
