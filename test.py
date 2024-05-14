e64fa1f2846d993fad6f6ecaa51ac136c16fe8ad
Enkidu93 <lowryec17@gcc.edu>
2024-05-14 09:47:49 -0400

    Debug

--- test.py
+++ test.py
@@ -4,14 +4,12 @@ import subprocess
 st.title("THIS IS AN EXAMPLE")
 if st.button("Run subprocess"):
     ret = subprocess.run("pip list", shell=True, capture_output=True, text=True)
-    print(ret.stdout)
+    st.write(ret.stdout)
     ret = subprocess.run("which python", shell=True, capture_output=True, text=True)
-    print(ret.stdout)
+    st.write(ret.stdout)
     ret = subprocess.run(
         "python -m module.script", shell=True, capture_output=True, text=True
     )
-    print(ret.stdout)
-    print(ret.stderr)
     st.write(ret.returncode)
     st.write(ret.stdout)
-    st.write(ret.stderr)
+    st.error(ret.stderr)
