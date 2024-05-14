a5f9b3b1bad44d0d6c7e9c924152bcbe823919bb
Enkidu93 <lowryec17@gcc.edu>
2024-05-14 09:37:04 -0400

    Debug cmnds

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import subprocess
 
-st.title('THIS IS AN EXAMPLE')
-if st.button('Run subprocess'):
-    ret = subprocess.run('python script.py')
+st.title("THIS IS AN EXAMPLE")
+if st.button("Run subprocess"):
+    subprocess.run("pip list", shell=True, capture_output=True, text=True)
+    subprocess.run("which python", shell=True, capture_output=True, text=True)
+    ret = subprocess.run(
+        "python -m module.script", shell=True, capture_output=True, text=True
+    )
     st.write(ret.returncode)
     st.write(ret.stdout)
     st.write(ret.stderr)
